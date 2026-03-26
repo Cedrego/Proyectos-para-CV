12f83c21036492ecf598b0e28f2d8971a65f90a2
Thiago Cedrés <104656328+Cedrego@users.noreply.github.com>
2026-03-26 20:24:05 -0300

    Prueba inicial de customtinker

--- Calculadora Python/main.py
+++ Calculadora Python/main.py
@@ -1,1 +1,16 @@
-print("¡Proyecto Python listo!")
+import customtkinter
+
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+        self.geometry("400x150")
+
+        self.button = customtkinter.CTkButton(self, text="my button", command=self.button_callbck)
+        self.button.pack(padx=20, pady=20)
+
+    def button_callbck(self):
+        print("button clicked")
+
+app = App()
+app.mainloop()
+##https://customtkinter.tomschimansky.com/tutorial/grid-system
