a53d2cc25aada095a6b181a4bfaf9303dc5e333c
Desckars <enzbrun8@gmail.com>
2026-04-14 22:55:48 -0300

    Inicio

--- Clicker/main.py
+++ Clicker/main.py
@@ -1,1 +1,18 @@
 import pyautogui
+
+import customtkinter as ctk
+from tkinter  import messagebox
+
+root = ctk.CTk()    
+root.title("Clicker | Hecho por Thiago Cedrés y Enzo Brun")
+root.geometry("500x500")
+root.resizable(0, 0)
+title_label = ctk.CTkLabel(root, text="Clicker | Hecho por Thiago Cedrés y Enzo Brun", font=("Arial", 12), text_color="#565656" )
+title_label.grid(row=0, column=0, columnspan=2, pady=(0, 0))
+
+color_texto = "000000"
+color_boton = "#D9D9D9"
+
+#def menu():
+    #Intervalo de tiempo entre clicks
+    
